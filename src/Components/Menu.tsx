0e36598630d02455c8c6670cd1009ebe322cf330
Devin Tripp <devinshawntripp@gmail.com>
2022-08-14 17:57:19 -0500

    changes to logo

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,36 +1,34 @@
 import { linkSync } from "fs";
 import * as React from "react";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 import { appSelector, loadAppData } from "../store/slices/app-slice";
-import { store } from '../store/store'
-import { useDispatch } from 'react-redux'
+import { store } from "../store/store";
+import { useDispatch } from "react-redux";
+import logo from "../images/whyareyoubuyingthislogo.png";
 import "./Menu.css";
 
 const Menu = (props: {
   brand: { name: string; to: string };
   links: Array<{ name: string; to: string }>;
 }) => {
-
   type RootState = ReturnType<typeof store.getState>;
   const { brand, links } = props;
-  const app = useSelector(appSelector)
+  const app = useSelector(appSelector);
   const dispatch = useDispatch();
 
-
-
-  const handleLogout = () =>{
-    dispatch(loadAppData({
-      id: "",
-      email: "",
-      admin: false,
-      vouchers: 0,
-      idsSaved: [0]
-    }))
+  const handleLogout = () => {
+    dispatch(
+      loadAppData({
+        id: "",
+        email: "",
+        admin: false,
+        vouchers: 0,
+        idsSaved: [0],
+      })
+    );
 
     localStorage.setItem("auth-token", "");
-  }
-
-
+  };
 
   const NavLinks: any = () =>
     links.map((link: { name: string; to: string }) => (
@@ -43,16 +41,32 @@ const Menu = (props: {
     <>
       <div className="Navbar">
         <a className="Brand" href={brand.to}>
-          {brand.name}
+          <img
+            style={{
+              maxHeight: "90px",
+              maxWidth: "120px",
+              marginRight: "-50px",
+              marginBottom: "-15px",
+              marginTop: "-25px",
+            }}
+            src={logo}
+          />
+          {/* {brand.name} */}
         </a>
         <NavLinks />
         <a></a>
-        {app.email != "" ? 
-          <><div className="Username">{app.email}</div><a className="Logout" onClick={handleLogout}>Logout</a></>
-        : 
+        {app.email != "" ? (
+          <>
+            <div className="Username">{app.email}</div>
+            <a className="Logout" onClick={handleLogout}>
+              Logout
+            </a>
+          </>
+        ) : (
           <a className="Login" href="/Login">
-          Login/Register
-        </a>}
+            Login/Register
+          </a>
+        )}
       </div>
     </>
   );
